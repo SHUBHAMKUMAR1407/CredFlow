ad015612d0607bd30b4ade853e2894d30f69081b
Shubham Kumar <sky143c@gmail.com>
2026-05-15 02:20:57 +0530

    switch SMTP transport to port 587 (STARTTLS) to bypass Render connection timeout

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -20,15 +20,12 @@ const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // Use SSL
+      port: 587,
+      secure: false, // true for 465, false for other ports (uses STARTTLS)
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000, // 10 seconds
-      greetingTimeout: 5000,
-      socketTimeout: 15000,
     });
 
     const mailOptions = {
